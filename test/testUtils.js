1dfb332486b0056bd9d6747477f7c616d5c0925c
Pouya Ataei <pouya.ataei.7@gmail.com>
2019-10-12 16:38:04 +1300

    initial commit

---
+++ test/testUtils.js
@@ -0,0 +1,11 @@
+/**
+ * Return node(s) with the given data-test attribute
+ * @function findByTestAttr
+ * @param {ShallowWrapper} wrapper - Enzyme Shallow Wrapper
+ * @param {String} val - Value of data-test attirbute for search
+ * @returns {ShallowWrapper}
+ */
+
+export const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`);
+};
