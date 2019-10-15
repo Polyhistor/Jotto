e461c2e1fbff268959323dd08498cfb32add20cf
Pouya Ataei <pouya.ataei.7@gmail.com>
2019-10-15 22:17:49 +1300

    written test for our action creator with redux thunk

--- src/integration.test.js
+++ src/integration.test.js
@@ -2,9 +2,34 @@ import { storeFactory } from "../test/testUtils";
 import { guessWord } from "./actions";
 
 describe("guessWord action dispatcher", () => {
+  const secretWord = "party";
+  const unssuccesfulGuess = "train";
   describe("no guessed words", () => {
-    test("update state correctly for unssuccesful guess", () => {});
-    test("update state correctly for succesful guess", () => {});
+    let store;
+    const initialState = { secretWord };
+    beforeEach(() => {
+      store = storeFactory(initialState);
+    });
+    test("update state correctly for unssuccesful guess", () => {
+      store.dispatch(guessWord(unssuccesfulGuess));
+      const newState = store.getState();
+      const expectedState = {
+        ...initialState,
+        success: false,
+        guessedWords: [{ guessedWord: unssuccesfulGuess, letterMatchCount: 3 }]
+      };
+      expect(newState).toEqual(expectedState);
+    });
+    test("update state correctly for succesful guess", () => {
+      store.dispatch(guessWord(secretWord));
+      const newState = store.getState();
+      const expectedState = {
+        ...initialState,
+        success: true,
+        guessedWords: [{ guessedWord: secretWord, letterMatchCount: 5 }]
+      };
+      expect(newState).toEqual(expectedState);
+    });
   });
   describe("some guessed words", () => {
     test("update state correctly for unssuccesful guess", () => {});
